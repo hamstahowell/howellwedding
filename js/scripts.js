6ba432fb6f8e1631be1e3a8dc60a62b6a044351f
hamstahowell <101473707+hamstahowell@users.noreply.github.com>
2025-01-06 15:31:44 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -214,7 +214,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40xxx'
+        if (MD5($('#invite_code').val()) !== 'd82bd82117a4bff275ea94f6ad56d023'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59ccxxx') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
