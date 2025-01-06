bbd947ec21b89dc8501b57008cd006d70f10b0d4
hamstahowell <101473707+hamstahowell@users.noreply.github.com>
2025-01-06 13:57:35 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,8 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'd82bd82117a4bff275ea94f6ad56d023'
-            && MD5($('#invite_code').val()) !== 'd82bd82117a4bff275ea94f6ad56d023') {
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbxHe0KaEwhst67Z6z4rdToVt-2ZPiSdcP1FpKubZ_eXiAniAvEKPpuVXCInh5Lci3fD/exec', data)
@@ -236,7 +236,6 @@ $(document).ready(function () {
     });
 
 });
-
 /********************** Extras **********************/
 
 // Google map
