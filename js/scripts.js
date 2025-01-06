947ce33a58cfa922c2d2395761c7bffef0f626de
hamstahowell <101473707+hamstahowell@users.noreply.github.com>
2025-01-06 13:46:18 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxIbAZXluh-btvgzMaNYKWz414aeiB8zCQsjKbwrwzrHpGabeWQ-y1wIq5pRZYcvKD7/exec, data)
+            $.post('https://script.google.com/macros/s/AKfycbxHe0KaEwhst67Z6z4rdToVt-2ZPiSdcP1FpKubZ_eXiAniAvEKPpuVXCInh5Lci3fD/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
