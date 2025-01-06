0aeade3e13c3e3476eba26afef0cdc0f120b0692
hamstahowell <101473707+hamstahowell@users.noreply.github.com>
2025-01-06 17:31:46 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -240,7 +240,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: -25.76668547476184, lng: 28.23582899607455};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
