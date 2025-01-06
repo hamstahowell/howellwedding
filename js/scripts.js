28c86e512f823bef6b2ac41dd861e9a18c7fe018
hamstahowell <101473707+hamstahowell@users.noreply.github.com>
2025-01-06 14:07:35 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -200,7 +200,7 @@ $(document).ready(function () {
             address: 'Cafe Mystique - c/o Jan Shoba &, Justice Mahomed St, Pretoria, 0011 ',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Nicholas or Rebecca."
         }
     });
 
